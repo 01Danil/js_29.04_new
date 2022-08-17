22d2610a99239c6b187fe215a79d6487b097e80a
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-17 18:42:52 +0300

    Example Exponentiation & Remainder

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let number_1 = "5";
-let number_2 = 4;
-console.log(number_1 - number_2);
+//Remainder
+let number_1 = 2;
+let number_2 = 20;
+console.log(number_1 % number_2);
 
 //унарные
 // console.log(+number_1);
@@ -8,3 +9,7 @@ console.log(number_1 - number_2);
 // console.log(number_1);
 
 //канкатинация "+" - это склеивание строк
+
+//Exponentiation
+console.log((-number_1)**3)
+console.log(-number_1*-number_1*-number_1);
