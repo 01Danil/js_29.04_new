0a1903cd977a1711b1dd70eb99596c253b18a419
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-17 19:27:23 +0300

    Add ekran symbol

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-//Increment
-let number_1 = 2;
-// console.log(number_1);
-// number_1++; //Postfix Increment
-// ++number_1; //Prefix Increment
-
-// console.log(number_1++);
-
-// console.log(number_1);
-
-let result = number_1++ * 10;
-console.log(number_1);
-console.log(result);
+let messUser = "Маша говорит:\u2721\n\t'Привет'!!!";
+console.log(messUser);
