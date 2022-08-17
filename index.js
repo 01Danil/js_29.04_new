4334af4c2ef34dacf9c1ddde1304c388ad9b6b4a
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-17 13:40:54 +0300

    Add declararion & initialization variable

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-console.log(102);
-console.log("Elon Musk");
-console.log(true);
-console.log(false);
-console.log(undefined);
-console.log(null);
+//variable
+/*
+	имя переменной только с буквы
+	начинается только с буквы или _ или $
+	лфтиница, без пробелов, цифры не первые
+	регистрозависимая user User UseR
+	camelCase
+	отвечает на вопрос Что в ней?
+*/
+
+let num1; // объявление
+console.log(num1);
+//console.log(num2);
+let num2 = 22;
+console.log(num2);
+num1 = 11; // инициализация
+num1 = 66;
+console.log(num1);
+const num3 = 33;
