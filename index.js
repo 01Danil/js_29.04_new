74ba908af1238a0cb7113f3f48b6aac279ed5e68
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-17 20:23:09 +0300

    Example &&, ||

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-let num1 = 10;
-console.log(num1>=10);
+//Logical AND (&&) умножение
+// Логическая && - возвращает либо первую лощь, либо последнюю правду
+
+//true&&true=true 		1*1=1
+//false&&true=false 	0*1=0
+//true&&false=false 	1*0=0
+//false&&false=true 	0*0=0
 
 
-console.log('test'>'Z');
+//Logical OR (||) сложение
+// Логическая && - возвращает либо первую правду, либо последнюю лошь
+
+//true||true=true 		1+1=2
+//false||true=true 		0+1=1
+//true||false=true 		1+0=1
+//false||false=false 	0+0=0
+
+let logic = 10 && 'false' && 45;
+console.log(logic);
+
+logic = 0 || false || 45;
+console.log(logic);
