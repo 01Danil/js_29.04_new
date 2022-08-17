14f622e43809f657da7b1ddc747dee95a1eb4fef
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-17 16:35:25 +0300

    Example uno/duo operators

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-let condition = false;
-console.log("value = ", condition);
+let number_1 = "5";
+let number_2 = 4;
+console.log(number_1 - number_2);
 
-if (condition) {
-  console.log("value = ", condition);
-}
+//унарные
+// console.log(+number_1);
+// console.log(-number_1);
+// console.log(number_1);
 
-
-
-/* 6-ть вариантов неправды
-	false
-	undefined
-	null
-	NaN
-	'' - пустая строка
-	0 +0 -0
-*/
+//канкатинация "+" - это склеивание строк
