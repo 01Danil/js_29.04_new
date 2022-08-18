bffa9b3248c73ac937e7a4da3d188b2d9838ddcf
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-18 15:52:10 +0300

    Example Prompt

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-//Assignment
+//Function
 
-let age = 10;
-//age = age + 5;
-age +=5
-
-let num = 3;
-//num = num * num;
-num *= num;
-
-let num2 = 56;
-num2 /= 2;
-
-let price = 10;
-price = (price + 5)*1.03;
+let str = prompt('Message', 'default');
+console.log(str);
