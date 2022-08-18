ba28723c4cdcb7e61027d9d61ad182ea510a80fc
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-18 17:05:53 +0300

    Add function calcSummaTwoNamberReqAtUser

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-// function requestNumberAdUser(){
-// 	let number = prompt('enter number', '10');
-// }
-
-//function expression
-const requestNumberAdUser = function () {
-  let number = prompt("enter number", "10");
+const calcSummaTwoNamberReqAtUser = function () {
+  let number1 = prompt("enter number 1", "10");
+  let number2 = prompt("enter number 2", "20");
+	//ошибка: если ничего не ввели (пустая строка) либо нажали отмена, либо ввели что-то, что не цифры
+	if(number1==='' || number2==='' || number1===null || number2===null || isNaN(Number(number1)) || isNaN(Number(number2))){
+		//true
+		//обработка ошибки
+		return 'error';
+	}else{
+		//false
+		let summa = Number(number1) + Number(number2);
+		return summa;
+	}
 };
-requestNumberAdUser;
-console.log(requestNumberAdUser());
+
+let summa = calcSummaTwoNamberReqAtUser();
+console.log(summa);
