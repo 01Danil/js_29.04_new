d193ac2eb8ce2610e15372f8c1f60dada16c2b96
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-19 10:13:53 +0300

    Add an initial function. Sample

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const calcSummaTwoNamberReqAtUser = function () {
   let number1 = prompt("enter number 1", "10");
   let number2 = prompt("enter number 2", "20");
-	//ошибка: если ничего не ввели (пустая строка) либо нажали отмена, либо ввели что-то, что не цифры
-	if(number1==='' || number2==='' || number1===null || number2===null || isNaN(Number(number1)) || isNaN(Number(number2))){
-		//true
-		//обработка ошибки
-		return 'error';
-	}else{
-		//false
-		let summa = Number(number1) + Number(number2);
-		return summa;
-	}
+  //ошибка: если ничего не ввели (пустая строка) либо нажали отмена, либо ввели что-то, что не цифры
+  if (
+    number1 === "" ||
+    number2 === "" ||
+    number1 === null ||
+    number2 === null ||
+    isNaN(Number(number1)) ||
+    isNaN(Number(number2))
+  ) {
+    //true
+    //обработка ошибки
+    return "error";
+  } else {
+    //false
+    let summa = Number(number1) + Number(number2);
+    return summa;
+  }
 };
 
 let summa = calcSummaTwoNamberReqAtUser();
