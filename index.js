53f152a91f90091a5013dd7d33d633b4fc301a10
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-19 15:44:48 +0300

    Add second task from Artem

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-// const number1 = prompt("enter number 1", "10");
-// const number2 = prompt("enter number 2", "20");
-
-/**
- * Description function
- * @param {null|string} num
- * @returns {boolean}
- */
-const isErrorInputNumber = function (num) {
-  return num === "" || num === "" || isNaN(Number(num));
+const calcSummaTwoNamberReqAtUser = function () {
+  const number = prompt("Укажите Ваш год рождения", "");
+  const thisYear = 2022;
+  //ошибка: если ничего не ввели (пустая строка) либо нажали отмена, либо ввели что-то, что не цифры
+  if (number === "" || number === null || isNaN(Number(number))) {
+    //true
+    //обработка ошибки
+    return "error";
+  } else {
+    //false
+    let summa = Number(thisYear) - Number(number);
+    return summa;
+  }
 };
-/**
- * Return summa two number.
- * @param {number} number1 default = 1
- * @param {number} number2 default = 2
- * @returns {number}
- */
-const calcSummaTwoNumber = function (number1=1, number2=2) {
-  return number1 + number2;
-};
-//debugger
-console.log(calcSummaTwoNumber(undefined, 45));
 
-// const check = isErrorInputNumber(number1) || isErrorInputNumber(number2);
-// if (check===false) { //!check - bad practic
-//   const summaUser = calcSummaTwoNumber(Number(number1), Number(number2));
-//   console.log(summaUser);
-// } else {
-//   console.log("not possible calc summa");
-// }
+let summa = calcSummaTwoNamberReqAtUser();
+alert(`Ваш возраст ${summa} лет!`);
