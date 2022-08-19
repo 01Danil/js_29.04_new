a19c35e62fe07171b2541a1282745627392f07a2
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-19 11:48:55 +0300

    Refactor calcSummaTwoNumberAtUser

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const calcSummaTwoNamberReqAtUser = function () {
-  let number1 = prompt("enter number 1", "10");
-  let number2 = prompt("enter number 2", "20");
-  //ошибка: если ничего не ввели (пустая строка) либо нажали отмена, либо ввели что-то, что не цифры
-	const error = number1 === "" ||	number2 === "" ||	number1 === null || number2 === null ||	isNaN(Number(number1)) ||	isNaN(Number(number2));
-  if (error) {
-    //true
-    //обработка ошибки
-    return "error";//exit
-  } 
-    //let summa = Number(number1) + Number(number2);
-		//return summa
-    return Number(number1) + Number(number2);
-  
-};
+const number1 = prompt("enter number 1", "10");
+const number2 = prompt("enter number 2", "20");
 
-let summa = calcSummaTwoNamberReqAtUser();
-console.log(summa);
+/**
+ * Description function
+ * @param {null|string} num
+ * @returns {boolean}
+ */
+const isErrorInputNumber = function (num) {
+  return num === "" || num === "" || isNaN(Number(num));
+};
+/**
+ * Return summa two number.
+ * @param {number} number1
+ * @param {number} number2
+ * @returns {number}
+ */
+const calcSummaTwoNumber = function (number1, number2) {
+  return number1 + number2;
+};
+const check = isErrorInputNumber(number1) || isErrorInputNumber(number2)
+if (check) {
+	console.log('not possible calc summa');
+} else {
+	const summaUser = calcSummaTwoNumber(Number (number1), Number (number2));
+	console.log(summaUser);
+}
+
+
