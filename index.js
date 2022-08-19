b8909d93382aacdd515a39db226a774c28a45b2a
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-08-19 15:51:01 +0300

    Change the task from Artem

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const calcSummaTwoNamberReqAtUser = function () {
-  const number = prompt("Укажите Ваш год рождения", "");
-  const thisYear = 2022;
-  //ошибка: если ничего не ввели (пустая строка) либо нажали отмена, либо ввели что-то, что не цифры
-  if (number === "" || number === null || isNaN(Number(number))) {
-    //true
-    //обработка ошибки
-    return "error";
-  } else {
-    //false
-    let summa = Number(thisYear) - Number(number);
-    return summa;
-  }
-};
-
-let summa = calcSummaTwoNamberReqAtUser();
+let yearOfBirthUser = prompt("Укажите Ваш год рождения", "");
+const thisYear = 2022;
+let summa = Number(thisYear) - Number(yearOfBirthUser);
 alert(`Ваш возраст ${summa} лет!`);
