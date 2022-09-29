0a7a02c7ebd9088eb2e8e9925ba4e630096705c5
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2022-09-29 15:23:58 +0300

    Add lessons 23/1

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-let yearOfBirthUser = prompt("Укажите Ваш год рождения", "");
-const thisYear = 2022;
-let summa = Number(thisYear) - Number(yearOfBirthUser);
-alert(`Ваш возраст ${summa} лет!`);
+// найти периметр квадрата
+// сторона *4
+//const sideSquare = prompt("enter side square", "10");
+/**
+ * 
+ * @param {number} side 
+ * @returns {null | number }
+ */
+const calkPerimetrSquare = function (side=0) {
+	if(side<0){
+		return null;
+	}
+  return side*4;
+};
+console.log(calkPerimetrSquare());
+//console.log(calkPerimetrSquare(sideSquare));
+console.log(calkPerimetrSquare(8));
+const result = calkPerimetrSquare(8);
+if(result===null){
+	alert("Введи положительное число")
+};
